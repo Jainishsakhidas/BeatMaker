e91c910c90a5244d6b10d69f4fb5c3da410457fa
Jainish Sakhidas <jainishsakhidas@gmail.com>
2021-06-30 09:14:12 +0530

    Looping over the pads

---
+++ app.js
@@ -0,0 +1,36 @@
+class Drumkit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.playBtn = document.querySelector(".play");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.index = 0;
+    this.bpm = 150;
+  }
+  activePad() {
+    this.classList.toggle("active");
+  }
+  repeat() {
+    let step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    console.log(step);
+    this.index++;
+  }
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    setInterval(() => {
+      this.repeat();
+    }, interval); //1000ms == 1s
+  }
+}
+
+const drumKit = new Drumkit();
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+});
+
+drumKit.playBtn.addEventListener("click", () => {
+  drumKit.start();
+});
